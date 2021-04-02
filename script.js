2d7f7e4ab070b48c34acfc1f378f0aa3b09930d4
Rahul Raina <rahulraina711@gmail.com>
2021-04-02 13:28:09 +0530

    added disconneting message as well

--- script.js
+++ script.js
@@ -5,16 +5,25 @@ const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
 const yourName = prompt('what is your name');
-appendMessage(`${yourName} joined the server`);
+appendMessage(`You joined the server`);
 socket.emit('new-user', yourName);
 
 socket.on('chat-message', data=>{
-    appendMessage(data);
+    appendMessage(`${data.name}: ${data.message}`);
+})
+
+socket.on('user-connected', username=>{
+    appendMessage(`${username} joined the server`);
+})
+
+socket.on('user-disconnected', username=>{
+    appendMessage(`${username} left the server`);
 })
 
 messageForm.addEventListener('submit', e=>{
     e.preventDefault();
     const message = messageInput.value;
+    appendMessage(`You: ${message}`);
     socket.emit('send-chat-message', message);
     messageInput.value = '';
 })
