148fadd1601ccbb17f5764ddbc183424bb50cd25
Rahul Raina <rahulraina711@gmail.com>
2021-04-02 13:11:00 +0530

    broadcasted text visible now

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const socket = io("http://localhost:3000");
 
+const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
+const yourName = prompt('what is your name');
+appendMessage(`${yourName} joined the server`);
+socket.emit('new-user', yourName);
+
 socket.on('chat-message', data=>{
-    console.log(data);
+    appendMessage(data);
 })
 
 messageForm.addEventListener('submit', e=>{
@@ -13,3 +18,9 @@ messageForm.addEventListener('submit', e=>{
     socket.emit('send-chat-message', message);
     messageInput.value = '';
 })
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+}
--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const io = require('socket.io')(3000, {
   });
 
 io.on('connection', socket=>{
-    socket.emit('chat-message', "hello world")
-    socket.on('send-chat-message', message=>{
-        console.log(message);
+    socket.on('send-chat-message', message =>{
+        socket.broadcast.emit('chat-message', message);
     })
 })
